8ec9f4d52528760ed3e164118497139e7551fca5
S-J-K-F <Skey@ftstudent.org>
2024-09-06 11:00:49 -0400

    Update bigboy.gd

--- scrip/bigboy.gd
+++ scrip/bigboy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -251.0
-const Jumps_Left = 2
+var Jumps_Left = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
