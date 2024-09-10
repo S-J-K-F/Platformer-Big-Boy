ea2845f8f1c435b4302954b514fa3feb2c623d93
Jameson McClellan <jameson.mcclellan99@gmail.com>
2024-09-10 12:58:56 -0400

    Update game_manager.gd

--- scrip/game_manager.gd
+++ scrip/game_manager.gd
@@ -6,3 +6,7 @@ var score = 0
 func add_point():
 	score += 1
 	score_label.text = "You've eaten " + str(score) + " onion rings!"
+func _on_Hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bigboy"):
+		print()
+		get_tree().change_scene("res://Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
